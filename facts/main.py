6970c41480a3572c3a013d7fa9d0c88880a33279
한경만 <kmhan@spectra.co.kr>
2024-05-10 08:08:04 +0900

    update

--- facts/main.py
+++ facts/main.py
@@ -1,9 +1,34 @@
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+embeddings = OpenAIEmbeddings()
 
-print(docs)
+emb = embeddings.embed_query("Hi there")
+
+# print(emb)
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=100)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(text_splitter=text_splitter)
+
+db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
+
+results = db.similarity_search_with_score(
+    "What is and interesting fact about the English language?",
+    # k=2
+)
+
+for result in results:
+    print("\n")
+    print(result[1])
+    print(result[0].page_content)
+
+# print(docs)
+# for doc in docs:
+#     print(doc.page_content)
+#     print("\n")
