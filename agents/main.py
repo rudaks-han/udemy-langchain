cdd96df1147f73af2733ec940c64ed7f33c99973
rudaks <rudaks94@gmail.com>
2024-05-11 20:09:23 +0900

    update

---
+++ agents/main.py
@@ -0,0 +1,53 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
+)
+from langchain.schema import SystemMessage
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from dotenv import load_dotenv
+
+from tools.sql import run_query_tool, list_tables, describe_table_tool
+from tools.report import write_report_tool
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+tables = list_tables();
+# print(tables)
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite databases.\n"
+            f"The database has tables of : {tables}\n"
+            "Do not make any assumptions about tables exists or"
+            "or what columns exists. Instead, use the 'describe_tables' function"
+        )),
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ],
+)
+
+tools = [
+    run_query_tool,
+    describe_table_tool,
+    write_report_tool
+]
+
+agent = OpenAIFunctionsAgent(
+    llm=chat,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    verbose=True,
+    tools=tools
+)
+
+# agent_executor("How many users are in the databases?")
+# agent_executor("How many users have provided a shipping address?")
+agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
---
+++ agents/tools/sql.py
@@ -0,0 +1,48 @@
+import sqlite3
+from pydantic.v1 import BaseModel
+from typing import List
+from langchain.tools import Tool
+
+conn = sqlite3.connect("db.sqlite")
+
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    # return rows
+    return "\n".join([row[0] for row in rows if row[0] is not None])
+
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
+
+def run_sqlite_query(query):
+    c = conn.cursor()
+    try:
+        c.execute(query)
+        return c.fetchall()
+    except sqlite3.OperationalError as err:
+        return f"The following error occurred: {str(err)}"
+
+run_query_tool = Tool.from_function(
+    name="run_sqlite_query",
+    description="Run a sqlite query.",
+    func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema
+)
+
+def describe_table(table_names):
+    c = conn.cursor()
+    tables = ', '.join("'" + table + "'" for table in table_names)
+    rows = c.execute(f"SELECT sql FROM sqlite_master WHERE name IN ({tables});")
+    return '\n'.join(row[0] for row in rows if row[0] is not None)
+
+class DescribeTableArgsSchema(BaseModel):
+    table_names: List[str]
+
+describe_table_tool = Tool.from_function(
+    name="describe_table",
+    description="Given a list of table names, returns the schema of those tables.",
+    func=describe_table,
+    args_schema=DescribeTableArgsSchema
+)
