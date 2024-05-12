db907e06dca092586037c244bb71da523b5be3fd
한경만 <kmhan@spectra.co.kr>
2024-05-13 08:10:34 +0900

    update

--- pycode/main.py
+++ pycode/main.py
@@ -1,52 +1,22 @@
-# from langchain.llms import OpenAI
-from langchain_openai import OpenAI
+from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
-from dotenv import load_dotenv
-import argparse
+from langchain.chains import LLMChain
 
-load_dotenv()
+api_key = "sk-xxx"
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="return a list of numbers")
-parser.add_argument("--language", default="python")
-args = parser.parse_args()
-
-llm = OpenAI()
+llm = OpenAI(openai_api_key=api_key)
 
 code_prompt = PromptTemplate(
-    input_variables=["task", "language"],
-    template="Write a very short {language} function that will {task}."
-)
-test_prompt = PromptTemplate(
-    input_variables=["language", "code"],
-    template="Write a test code for the following {language} code:\n{code}"
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
 )
 
 code_chain = LLMChain(
     llm=llm,
     prompt=code_prompt,
-    output_key="code"
-)
-test_chain = LLMChain(
-    llm=llm,
-    prompt=test_prompt,
-    output_key="test"
 )
 
-chain = SequentialChain(
-    chains=[code_chain, test_chain],
-    input_variables=["task", "language"],
-    output_variables=["test", "code"]
-)
+# result = llm("Write a very very short poem")
+result = code_chain({"language": "python", "task": "return a list of numbers"})
 
-result = chain({
-    "language": args.language,
-    "task": args.task
-})
-
-print(">>>>>> GENERATED CODE:")
-print(result["code"])
-
-print(">>>>>> GENERATED TEST:")
-print(result["test"])
+print(result)
