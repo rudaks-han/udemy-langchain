822fedae7a8bdf88ad71b29e515194690946a9d0
한경만 <kmhan@spectra.co.kr>
2024-05-14 18:21:55 +0900

    update

--- tchat/main.py
+++ tchat/main.py
@@ -1,13 +1,37 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+# memory_key는
+# return_messages는 string이 아니라 object로 리턴하는 것을 의미
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True,
+)
 
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}"),
     ],
 )
 
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
+
 while True:
     content = input(">> ")
 
-    print(f"You entered: {content}")
+    result = chain({"content": content})
+    print(result["text"])
